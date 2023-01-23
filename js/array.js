638721e287c18cd7b7a468ad1f9a8ecfc0f093a1
PersieB <persiebrown285@gmail.com>
2023-01-23 13:06:57 +0000

    create bars

--- js/array.js
+++ js/array.js
@@ -1,3 +1,10 @@
 // array of random numbers between 0 and 100
-const array = Array(100).fill().map(() => Math.floor(100 * Math.random()));
+const array = Array(10).fill().map(() => Math.floor(100 * Math.random()));
 console.log(array);
+
+// creating bars from the random numbers generated
+for(let i=0; i < array.length; i++){
+    const bar = document.createElement("div").className = "barCharts";
+    let div_height = i*5 + "px";
+    bar.style.height = div_height;
+}
