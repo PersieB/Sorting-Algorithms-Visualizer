df1e467b7ecc3d1c0e2a00e8f271f9ef54af6758
PersieB <persiebrown285@gmail.com>
2023-01-26 16:29:58 +0000

    new updates

--- js/array.js
+++ js/array.js
@@ -1,10 +1,22 @@
 // array of random numbers between 0 and 100
-const array = Array(10).fill().map(() => Math.floor(100 * Math.random()));
-console.log(array);
-
-// creating bars from the random numbers generated
-for(let i=0; i < array.length; i++){
-    const bar = document.createElement("div").className = "barCharts";
-    let div_height = i*5 + "px";
-    bar.style.height = div_height;
+function createBars(){
+    const array = Array(10).fill().map(() => Math.floor(100 * Math.random()));
+    console.log(array);
+    
+    // creating bars from the random numbers generated
+    const bardiv = document.getElementsByClassName("row");
+    for(let i=0; i < array.length; i++){
+        const bar = document.createElement("div").className = "barCharts";
+        let div_height = i*5 + "px";
+        bar.style.height = div_height;
+        bardiv.appendChild(bar);
+    }
 }
+
+function newArray(){
+    const element = document.getElementById("newArray");
+    element.addEventListener("click", createBars);
+}
+
+createBars();
+
